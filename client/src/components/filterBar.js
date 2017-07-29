5a3a95e24f0f3ab9a0f550551f4218c65c7f0daf
nkernis <nkernis@gmail.com>
2017-07-29 12:02:09 -0400

    async loading googple api script

--- client/src/components/filterBar.js
+++ client/src/components/filterBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Button, Form, Menu } from 'semantic-ui-react'
 import Geosuggest from 'react-geosuggest'
+import scriptLoader from 'react-async-script-loader'
 import '../css/filter_bar.css'
 
 class FilterBar extends Component {
@@ -39,20 +40,33 @@ class FilterBar extends Component {
     })
   }
 
+
+  loading = () => {
+    if (this.props.isScriptLoadSucceed) {
+      return (
+        <Form as={Menu} className="filter-container">
+          <Form.Group widths='equal'>
+            <Form.Input name="start_date" type="date" label="Start Date" onChange={this.chnageHandler}/>
+            <Form.Input name="end_date" type="date" label="End Date" onChange={this.chnageHandler}/>
+            <Geosuggest placeholder="Location" country="us" onSuggestSelect={this.locationHandler}/>
+            {/* <Form.Select name="issue_type" label="Issue Type" placeholder="Issue Type" options={this.issue_options} onChange={this.chnageHandler} /> */}
+            {/* <Form.Select name="num_complaints" label="Number of Complaints" placeholder="Number of Complaints" options={this.complain_options} onChange={this.chnageHandler} /> */}
+            <Button className="submit-button" onClick={this.submitHandler}>Submit</Button>
+          </Form.Group>
+        </Form>
+      )
+    } else {
+      return <h2>Loading...</h2>
+    }
+  }
+
   render() {
     return (
-      <Form as={Menu} className="filter-container">
-        <Form.Group widths='equal'>
-          <Form.Input name="start_date" type="date" label="Start Date" onChange={this.chnageHandler}/>
-          <Form.Input name="end_date" type="date" label="End Date" onChange={this.chnageHandler}/>
-          <Geosuggest placeholder="Location" country="us" onSuggestSelect={this.locationHandler}/>
-          {/* <Form.Select name="issue_type" label="Issue Type" placeholder="Issue Type" options={this.issue_options} onChange={this.chnageHandler} /> */}
-          {/* <Form.Select name="num_complaints" label="Number of Complaints" placeholder="Number of Complaints" options={this.complain_options} onChange={this.chnageHandler} /> */}
-          <Button className="submit-button" onClick={this.submitHandler}>Submit</Button>
-      </Form.Group>
-      </Form>
+      <div>
+        {this.loading()}
+      </div>
     )
   }
 }
 
-export default FilterBar
+export default scriptLoader('https://maps.googleapis.com/maps/api/js?key=AIzaSyAMjLOSB8OGzVknNO5nLNOE-b0RXYq0qFc&libraries=places')(FilterBar)
