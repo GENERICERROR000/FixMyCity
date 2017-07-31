b29c676ba9abe4ad8fb63f7be2b4cae23e567774
nkernis <nkernis@gmail.com>
2017-07-31 00:39:27 -0400

    image zoom on click

--- client/src/components/displayIssue.js
+++ client/src/components/displayIssue.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import { Button, Header } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
+import ImageZoom from 'react-medium-image-zoom'
 import GoogleMap from './googleMap'
 
 // TODO: Make sure Media can handle multiple Images
 
 const MoreInfo = (props) => {
+  this.images = () => {
+    return (
+      <ImageZoom
+        image={{
+          src: `${props.data.media}`,
+          alt: 'No Pictures Provided',
+          className: 'pic',
+        }}
+        zoomImage={{
+          src: `${props.data.media}`,
+          alt: 'No Pictures Provided'
+        }}
+      />
+    )
+  }
+
   this.convertDate = () => {
     return new Date(Date.parse(props.data.posted_on)).toUTCString().replace(/\s*(GMT|UTC)$/, "")
   }
@@ -21,7 +38,7 @@ const MoreInfo = (props) => {
           <div>
             <h3>Posted on: {this.convertDate()}</h3>
             <h4>{props.data.tweet_content}</h4>
-            <img className="pic" src={props.data.media} alt="No Pictures Provided" />
+            {this.images()}
           </div>
         </div>
         <div className="map">
